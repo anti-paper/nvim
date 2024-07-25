9144cd33938ed3ef197bca2ab35e91221079ec28
anti-paper <nakaji361ll@gmail.com>
2024-07-25 21:08:53 +0900

    バッファ選択クローズのキーバインド設定

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -30,4 +30,5 @@ require'bufferline'.setup({
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', '<C-p>', '<Cmd>BufferLinePickClose<CR>', {})
 
