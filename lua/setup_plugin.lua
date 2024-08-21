033c5b5ac5273f38c618cd04c77e1d7e6eabf457
anti-paper <nakaji361ll@gmail.com>
2024-08-22 01:57:35 +0900

    RainbowCSV追加・キーバインド追加

--- lua/setup_plugin.lua
+++ lua/setup_plugin.lua
@@ -3,7 +3,7 @@ local fn = vim.fn
 local jetpackfile = fn.stdpath('data') .. '/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim'
 local jetpackurl = 'https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim'
 if fn.filereadable(jetpackfile) == 0 then
-  fn.system('curl -fsSLo ' .. jetpackfile .. ' --create-dirs ' .. jetpackurl)
+    fn.system('curl -fsSLo ' .. jetpackfile .. ' --create-dirs ' .. jetpackurl)
 end
 
 
@@ -12,20 +12,20 @@ vim.cmd('packadd vim-jetpack')
 
 
 require('jetpack.paq') {
-    {'tani/vim-jetpack', opt = 1}, -- bootstrap
+    { 'tani/vim-jetpack',                opt = 1 }, -- bootstrap
     'navarasu/onedark.nvim',
-    {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
-    {'nvim-tree/nvim-tree.lua',
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    { 'nvim-tree/nvim-tree.lua',
         requires = {
-          'nvim-tree/nvim-web-devicons',
+            'nvim-tree/nvim-web-devicons',
         }
     },
     'nvim-lua/plenary.nvim',
-    {'nvim-telescope/telescope.nvim', tag = '0.1.4' },
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
     'nvim-tree/nvim-web-devicons',
     {
-      'nvim-lualine/lualine.nvim',
-      requires = 'kyazdani42/nvim-web-devicons'
+        'nvim-lualine/lualine.nvim',
+        requires = 'kyazdani42/nvim-web-devicons'
     },
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
@@ -39,7 +39,7 @@ require('jetpack.paq') {
     "neovim/nvim-lspconfig",
     'yamatsum/nvim-cursorline',
     'pechorin/any-jump.vim',
-    {'numToStr/Comment.nvim', config = function() require('Comment').setup() end},
+    { 'numToStr/Comment.nvim',         config = function() require('Comment').setup() end },
     'norcalli/nvim-colorizer.lua',
     'dinhhuy258/git.nvim',
     'lewis6991/gitsigns.nvim',
@@ -51,8 +51,8 @@ require('jetpack.paq') {
     'echasnovski/mini.indentscope',
     'akinsho/toggleterm.nvim',
     {
-      'nvimtools/none-ls.nvim',
-      requires = {'nvim-lua/plenary.nvim', 'vim-test/vim-test'},
+        'nvimtools/none-ls.nvim',
+        requires = { 'nvim-lua/plenary.nvim', 'vim-test/vim-test' },
     },
+    'mechatroner/rainbow_csv',
 }
-
